bf383032e14ec29d7d79ce419a9b95fac21d5f92
emilynnj <emilynnj14@gmail.com>
2026-03-15 23:06:42 -0400

    ?

--- client/src/hooks/useClerkUserRole.ts
+++
@@ -1,9 +0,0 @@
-import { useUser } from '@clerk/clerk-react';
-
-export const useClerkUserRole = () => {
-  const { user, isLoaded } = useUser();
-  if (!isLoaded || !user) {
-    return null;
-  }
-  return user.publicMetadata.role || null;
-};
